343db317e4faceb9b1bc82235b5a314e8cea5a6c
cs148 <behrens.adam@gmail.com>
2016-08-01 18:21:16 -0700

    [chore] add external to gitignore

---
+++ build/external/glew-cmake/CMakeFiles/libglew_static.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/cs148/Desktop/group-graphics/external/glew-cmake/src/glew.c" "/home/cs148/Desktop/group-graphics/build/external/glew-cmake/CMakeFiles/libglew_static.dir/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "GLEW_NO_GLU"
+  "GLEW_STATIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glew-cmake/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
