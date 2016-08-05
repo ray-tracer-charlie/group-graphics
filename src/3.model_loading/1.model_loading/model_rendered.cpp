48f25de8e5349a5cc75479398667687e36d59878
Charles Huyi <chuyi@stanford.edu>
2016-08-05 13:17:55 -0700

    phelps, starting edge detection

--- src/3.model_loading/1.model_loading/model_rendered.cpp
+++ src/3.model_loading/1.model_loading/model_rendered.cpp
@@ -196,7 +196,7 @@ int main()
     // Load, create texture and generate mipmaps
     int width, height;
 
-    unsigned char* image = SOIL_load_image(FileSystem::getPath("resources/images/dog.jpg").c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+    unsigned char* image = SOIL_load_image(FileSystem::getPath("resources/images/phelps.jpg").c_str(), &width, &height, 0, SOIL_LOAD_RGB);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
